76e1b9af0cad6d8e9a8b1493b7fc4b699f32ee7f
JulianaBetancurth <julianabl18@hotmail.com>
2023-04-05 18:33:34 -0500

    Agregar archivos de proyecto.

---
+++ AppDispensador/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dispensador;
+namespace AppDispensador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dispensadora dispensador = new Dispensadora();
+
+            while (true)
+            {
+                Console.WriteLine("Bienvenidos a la dispensadora de Juliana");
+               
+                Console.WriteLine(dispensador.listarProducto());
+
+                Console.WriteLine("1. Agregar producto");
+                Console.WriteLine("2. Modificar producto");
+                Console.WriteLine("3. Eliminar producto");
+                Console.WriteLine("4. Comprar producto");
+                string opcion = Console.ReadLine();
+
+                switch(opcion) 
+                {
+                    case "1":
+                        Producto producto = new Producto();
+                        Console.Write("Codigo");
+                        producto.Codigo = Console.ReadLine();
+
+                        Console.Write("Nombre");
+                        producto.Nombre = Console.ReadLine();
+
+                        Console.Write("Categoria");
+                        producto.Categoria = Console.ReadLine();
+
+                        Console.Write("Cantidad");
+                        producto.Cantidad = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Valor");
+                        producto.Valor = double.Parse(Console.ReadLine());
+
+                        dispensador.agregarProducto(producto);
+
+                        break;
+                        case "2":
+                        // Mostrar los datos del producto
+                        
+                        Producto producto1 = new Producto();
+
+                        
+                        Console.WriteLine("Datos del producto original:");
+                        Console.WriteLine("Nombre: {0}", producto1.Nombre);
+                        Console.WriteLine("Categoría: {0}", producto1.Categoria);
+                        Console.WriteLine("Cantidad: {0}", producto1.Cantidad);
+                        Console.WriteLine("Valor: {0}", producto1.Valor);
+                        Console.WriteLine();
+
+                        // Pedir al usuario los nuevos datos del producto
+                        Console.WriteLine("Ingrese los nuevos datos del producto:");
+                        Console.Write("Nombre: ");
+                        string nuevoNombre = Console.ReadLine();
+                        Console.Write("Categoría: "); 
+                        string nuevaCategoria = Console.ReadLine();
+                        Console.Write("Cantidad: ");
+                        int nuevaCantidad = int.Parse(Console.ReadLine());
+                        Console.Write("Valor: ");
+                        double nuevoValor = double.Parse(Console.ReadLine());
+
+                        // Actualizar el objeto Producto con los nuevos datos
+                        Producto.modificarProducto(nuevoNombre, nuevaCategoria, nuevaCantidad, nuevoValor);
+
+                        // Mostrar los datos del producto actualizado
+                        Console.WriteLine("Datos del producto actualizado:");
+                        Console.WriteLine("Nombre: {0}", producto1.Nombre);
+                        Console.WriteLine("Categoría: {0}", producto1.Categoria);
+                        Console.WriteLine("Cantidad: {0}", producto1.Cantidad);
+                        Console.WriteLine("Valor: {0}", producto1.Valor);
+
+                        break; 
+
+                        case "3":
+
+                        Console.Write("Codigo");
+                        string codigo = Console.ReadLine();
+
+                        dispensador.eliminarProducto(codigo);
+
+                        break;
+
+                        case "4":
+
+                        Console.Write("Codigo");
+                        string codigo_venta= Console.ReadLine();
+
+                        Console.Write("Monedas solo de (1000-500-200-100)");
+                        dispensador.Pago = Console.ReadLine();
+
+                        Producto pcomprado= dispensador.vender(codigo_venta); 
+
+                        if( pcomprado == null) 
+                        {
+                            Console.WriteLine("No se pudo sacar el producto");
+                        
+                        }
+                        else 
+                        {
+                            Console.WriteLine("Su producto es {0} y la devuelta es {1}", pcomprado.Codigo, pcomprado.Cambio);
+                                                
+                        }
+
+                        break;  
+
+                }
+
+                Console.WriteLine("Desea continuar si/no");
+
+                if(Console.ReadLine() == "no") 
+                {
+                    break;
+                
+                }
+
+
+            } 
+
+
+
+            
+
+        }
+    }
+}
---
+++ Dispensador/Dispensadora.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispensador
+{
+    public class Dispensadora
+    {
+        public List<Producto> Productos { get; set; }
+        public string Pago { get; set; }
+
+        public Dispensadora()
+        {
+            this.Productos = new List<Producto>();
+            Producto cocacola = new Producto();
+            cocacola.Codigo = "01";
+            cocacola.Nombre = "Coca Cola";
+            cocacola.Categoria = "B";
+            cocacola.Cantidad = 5;
+            cocacola.Valor = 1000;
+
+            Producto papas = new Producto();
+            papas.Codigo = "02";
+            papas.Nombre = "Margaritas";
+            papas.Categoria = "M";
+            papas.Cantidad = 2;
+            papas.Valor = 500;
+
+            this.Productos.Add(cocacola);
+            this.Productos.Add(papas);
+
+        }
+
+        public int validaProducto(string codigo)
+        {
+            int encontro = -1;
+
+
+            for (int i = 0; i < this.Productos.Count; i++)
+            {
+                if (this.Productos[i].Codigo == codigo)
+                {
+                    encontro = i;
+                }
+
+            }
+
+            return encontro;
+
+        }
+
+        public void modificarProducto(string nombre, string categoria, int cantidad, double valor) 
+        { 
+            foreach ( Producto item in this.Productos) 
+            {
+                if(item.Nombre == nombre) 
+                {
+                    
+                    item.Categoria = categoria;
+                    item.Cantidad = cantidad;
+                    item.Valor = valor;
+
+                }
+            
+            }
+                     
+        
+        }
+
+        public bool agregarProducto(Producto producto)
+        {
+            int enc = (this.validaProducto(producto.Codigo));
+            if (enc >= 0)
+            {
+                this.Productos[enc].sumarCantidad(producto.Cantidad);
+
+            }
+            else
+            {
+                this.Productos.Add(producto);
+
+            }
+            return true;
+
+
+        }
+
+        public bool eliminarProducto(string codigo)
+        {
+            int enc = (this.validaProducto(codigo));
+
+            if (enc >= 0)
+            {
+                this.Productos.RemoveAt(enc);
+                return true;
+
+            }
+
+            return false;
+        }
+
+        public double validarMonedas(string[] monedas)
+        {
+            double total = 0;
+            foreach (string item in monedas)
+            {
+                try
+                {
+                    total += float.Parse(item);
+
+                }
+                catch (Exception ) 
+                { 
+                
+                
+                }
+
+
+
+
+
+            }
+            return total;
+
+
+        }
+
+        // 1000-500-200-100
+
+        public Producto vender(string codigo)
+        {
+            int enc = (this.validaProducto(codigo));
+
+            if (enc >= 0)
+            {
+                if (this.Productos[enc].validarCantidad())
+                {
+                    string[] monedas = this.Pago.Split('-');
+
+                    double total = this.validarMonedas(monedas);
+
+                    if (this.Productos[enc].validarValor(total))
+                    {
+                        this.Productos[enc].restarProducto();
+
+                        return this.Productos[enc];
+
+                    }
+
+                }
+            }
+
+            return null;
+
+        }
+
+        public string listarProducto() 
+        { string lista = "";
+            foreach (Producto item in this.Productos) 
+            {
+                lista += item.Codigo + " " + item.Nombre + " " + item.Categoria + " " + item.Cantidad + " " + item.Valor + "\n";
+                        
+            }
+
+            return lista;
+                
+        }
+
+    }
+}
